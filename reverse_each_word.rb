3519d22e0c85df8e393d09628086724034b71c28
Fer Hernandez <fh.1111@gmail.com>
2018-10-28 22:07:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,4 @@
-def reverse_each_word (sentence)
 
-  sentence=sentence.split('')
-  rev_string = []
-
-  sentence.map do |letter|
-    rev_string.unshift(letter)
-
-  end
-
-  new_sentence = rev_string.join()
-  puts new_sentence
-
-end
 
 
 
