04ef2383ff075d0b81cd55da4547a79d3fe38f32
Fer Hernandez <fh.1111@gmail.com>
2018-10-28 22:06:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,3 +14,19 @@ def reverse_each_word (sentence)
 end
 
 reverse_each_word("Hello there, and how are you?")
+
+
+def reverse_each_word (sentence)
+
+  sentence_array=sentence.split(' ')
+  rev_string = []
+
+  sentence_array.map do |word|
+    rev_string << word.reverse
+
+  end
+
+  new_sentence = rev_string.join()
+  puts new_sentence
+
+end
