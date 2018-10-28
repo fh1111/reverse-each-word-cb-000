2a07a5ceabb2df889577070c56a0d33da866f173
Fer Hernandez <fh.1111@gmail.com>
2018-10-28 21:54:23 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word (sentence)
+
+  string = sentence.split(" ")
+  rev_string
+
+  string.each do |word|
+    rev_string.unshift(word)
+
+  end
+
+  puts rev_string
+
+end
+
+ reverse_each_word("Hi my name is fernando")
+
+end
